f458ef6ae88bb7ce6d56c38e460047190571607a
bashyauri <80903696+bashyauri@users.noreply.github.com>
2024-02-06 20:15:13 +0100

    update registercontroller and request

--- app/Http/Controllers/User/RegisterController.php
+++ app/Http/Controllers/User/RegisterController.php
@@ -2,10 +2,26 @@
 
 namespace App\Http\Controllers\User;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Services\User\AuthService;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\User\RegisterRequest;
 
 class RegisterController extends Controller
 {
-    //
+    public function __construct(protected AuthService $authService)
+    {
+    }
+    public function register(RegisterRequest $request)
+    {
+        try {
+            $data = $this->authService->register($request->validated());
+            return $this->resourceCreated(new UserResource($data), 'Registeration Successful. Please check your email for verification token');
+        } catch (CustomException $ex) {
+            return $this->errorResponse($ex->getMessage(), 401);
+        } catch (Exception $ex) {
+            Log::error($ex->getMessage());
+            return $this->errorResponse("Something went wrong", 401);
+        }
+    }
 }
---
+++ app/Http/Requests/User/RegisterRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests\User;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => ['required', 'string'],
+            'last_name' => ['required', 'string'],
+            'email' => ['required', 'email', 'unique:users'],
+            'password' => ['required', 'min:8', 'confirmed'],
+        ];
+    }
+    public function messages(): array
+    {
+        return [
+            'email.unique' => 'An account with this email address already exist.',
+            'phone.unique' => 'An account with this phone number already exist.',
+        ];
+    }
+}
