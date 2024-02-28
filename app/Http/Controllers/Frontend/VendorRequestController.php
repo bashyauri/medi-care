96fbc8a82d61251454a7302e5bbc0ab0a9db29fe
bashyauri <80903696+bashyauri@users.noreply.github.com>
2024-02-28 23:45:02 +0100

    add vendor request controller

--- app/Http/Controllers/Frontend/VendorRequestController.php
+++ app/Http/Controllers/Frontend/VendorRequestController.php
@@ -2,10 +2,37 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use Exception;
+use App\Traits\ResponseTrait;
+use App\Exceptions\CustomException;
+
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+
+use App\Http\Resources\Vendor\VendorResource;
+use App\Services\Vendor\VendorService as VendorVendorService;
+use App\Http\Requests\Vendor\UserVendorRequest as VendorUserVendorRequest;
 
 class VendorRequestController extends Controller
 {
-    //
+    use ResponseTrait;
+    public function __construct(protected VendorVendorService $vendorService)
+    {
+    }
+    public function store(VendorUserVendorRequest $request)
+    {
+        if (auth()->user()->role === 'vendor') {
+            return redirect()->back();
+        }
+        try {
+
+            $vendor = $this->vendorService->requestVendor($request, $request->validated());
+            return $this->successResponse("Vendor created", [
+                'vendor' => new VendorResource($vendor),
+            ], 201);
+        } catch (CustomException $ex) {
+            return $this->errorResponse($ex->getMessage());
+        } catch (Exception $ex) {
+            return $this->errorResponse("Something went wrong " . $ex->getMessage());
+        }
+    }
 }
