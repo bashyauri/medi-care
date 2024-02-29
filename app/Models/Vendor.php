3238b548fbd4baddae9c53bf3c70d25e6d9e6dd4
bashyauri <80903696+bashyauri@users.noreply.github.com>
2024-03-01 00:25:51 +0100

    adjust vendor and vendor address model

--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -14,4 +14,8 @@ class Vendor extends Model
     {
         return $this->belongsTo(User::class);
     }
+    public function vendorAddress()
+    {
+        return $this->hasOne(VendorAddress::class);
+    }
 }
--- app/Models/VendorAddress.php
+++ app/Models/VendorAddress.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class VendorAddress extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+
+    public function vendor()
+    {
+        return $this->belongsTo(Vendor::class);
+    }
 }
