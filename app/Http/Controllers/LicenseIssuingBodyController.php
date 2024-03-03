37b383a21f575507fb860225b9fb0ea71cd7e34d
bashyauri <80903696+bashyauri@users.noreply.github.com>
2024-03-04 00:08:26 +0100

    add license isuuing controller

---
+++ app/Http/Controllers/LicenseIssuingBodyController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Exception;
+use App\Traits\ResponseTrait;
+use Illuminate\Support\Facades\Log;
+use App\Services\LicenseIssuingBodyService;
+use App\Http\Resources\LicenseIssuingBodyResource;
+
+class LicenseIssuingBodyController extends Controller
+{
+    use ResponseTrait;
+    public function fetchAllLicenseIssuingBodies(LicenseIssuingBodyService $service)
+    {
+        try {
+            $countries = $service->fetchAllIssuingBodies();
+            return  $this->successResponse('Licensed Issuing Bodies fetched successfully', LicenseIssuingBodyResource::collection($countries));
+        } catch (Exception $e) {
+            Log::alert($e->getMessage());
+            return $this->errorResponse("Something went wrong");
+        }
+    }
+}
