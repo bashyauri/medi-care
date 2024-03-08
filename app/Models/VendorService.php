343ac37fd86040125d800103dcbb44eacb0e9b69
bashyauri <80903696+bashyauri@users.noreply.github.com>
2024-03-08 21:40:42 +0100

    added soft delete

--- app/Models/VendorService.php
+++ app/Models/VendorService.php
@@ -5,11 +5,12 @@ namespace App\Models;
 use App\Observers\VendorServiceObserver;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class VendorService extends Model
 {
-    use HasFactory;
+
+    use HasFactory, SoftDeletes;
     protected $fillable = [
         'service_type_id',
         'license_number',
